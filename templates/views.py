01186de4d0a6532496c02e70c9f6511c2b3857b5
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-14 10:38:18 +0530

    32557

--- templates/views.py
+++ templates/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import MenuItem
+
 def reservations_view(request):
     return render(request, 'reservations.html')
+
+def menu_view(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, "menu_list.html", {"menu_items": menu_items, "restaurant_name": "Your Restaurant Name"})
