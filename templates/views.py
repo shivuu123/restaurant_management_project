6a6dcdda1c8471561f86d9dbfe581ee14d91e0ad
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-18 09:41:36 +0530

    34391

--- templates/views.py
+++ templates/views.py
@@ -13,3 +13,14 @@ def home(request):
         'restaurant_name': settings.restaurant_name
     }
     return render(request, 'homepage.html')
+
+def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form':form})
+        
