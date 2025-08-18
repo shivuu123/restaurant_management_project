6baef1a249566d407979b08db6e9035936e8f816
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-18 09:25:49 +0530

    34392

--- templates/models.py
+++ templates/models.py
@@ -6,3 +6,12 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
