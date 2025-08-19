44944c253728983641525de9346895561e0bb21b
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-19 09:31:16 +0530

    34389

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_length=6, decimal_places=2)
+    image = models.imageField(upload_to='menu_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
