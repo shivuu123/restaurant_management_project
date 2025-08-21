ff42d627adbb688401dfa5c5a878e6ddd3e1142f
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-21 09:27:06 +0530

    35673

--- templates/models.py
+++ templates/models.py
@@ -3,6 +3,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
