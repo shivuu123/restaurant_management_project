70b6e8a07e3d3468681c0faf0658fc6b083c4af5
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-26 11:00:31 +0530

    38846

--- templates/forms.py
+++ templates/forms.py
@@ -5,4 +5,13 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True)
     email = forms.EmailField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True, min_length=10)
-   
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ['name', 'feedback_text']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your name'}),
+            'feedback_text': forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Write your feedback....'}),
+            }
+        
