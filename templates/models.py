c6ac8dd492db1cd17dd3a6e984e4787c6d1608ee
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-08-28 11:04:36 +0530

    39711

--- templates/models.py
+++ templates/models.py
@@ -44,4 +44,11 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
 
