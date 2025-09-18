2bcfb2d5081d96e22f79a90469ac0db34be138b3
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-18 09:19:10 +0530

    46314

--- orders/models.py
+++ orders/models.py
@@ -1,17 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
-# Create your models here.
-class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-
-class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    item = models.ForeignKey(Menu, on_delete=models.CASCADE)
-    qty = models.PositiveIntegerField()
+    def __str__(self):
+        return self.name
