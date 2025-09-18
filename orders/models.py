ad3d3cae27da7b344a550ea12c8d091b5e408d5a
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-18 09:24:12 +0530

    46313

--- orders/models.py
+++ orders/models.py
@@ -5,3 +5,18 @@ class OrderStatus(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders"
+    )
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.cusomer_name}"
