606b5513582a7eb9eb4cc8269dd2c3a21b51039b
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-18 09:15:34 +0530

    46315

---
+++ orders/utils.py
@@ -0,0 +1,23 @@
+import string
+import secrets
+from django.db import models
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.code
+
+def generate_coupon_code(length=10):
+    """
+    Generate a unique alphanumeric coupon code.
+    :param length: Length of the generated code (default: 10)
+    :return: A unique coupon code string
+    """
+    characters = string.ascii_uppercase + string.digits
+
+    while True:
+        code = ''.join(secrets.choice(characters) for _ in range(length))
+        if not Coupon.objects.filter(code=code).exists():
+            return code
