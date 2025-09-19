aa54101ee562ff91068ea438b5f5682a55a96f9e
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-19 11:57:02 +0530

    48284

--- home/serializers.py
+++ home/serializers.py
@@ -4,4 +4,10 @@ from .models import MenuItem
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
-        fields = ['id', 'name', 'price', 'description']
+        fields = ['id', 'name', 'price', 'description', 'is_available']
+
+    def validate_price(self, value):
+        """Ensure price is positive."""
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number.")
+        return value
