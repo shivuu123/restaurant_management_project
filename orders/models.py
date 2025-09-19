0d73e8587b2fc740adbbf5eba5c6d15e8359e5cc
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-19 12:41:46 +0530

    48287

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -20,3 +21,14 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.cusomer_name}"
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
