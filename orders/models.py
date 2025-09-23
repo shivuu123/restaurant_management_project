483271eb154d19aa6e9eaef12e11d6a0abd6b4bd
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-23 10:48:13 +0530

    50353

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from home.models import Product
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -8,18 +10,12 @@ class OrderStatus(models.Model):
 
 class Order(models.Model):
     customer_name = models.CharField(max_length=100)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-    status = models.ForeignKey(
-        OrderStatus,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name="orders"
-    )
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    order_items = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Order #{self.id} - {self.customer_name}"
+        return f"Order #{self.id} - {self.customer_username}"
 
 class ActiveOrderManager(models.Manager):
     def get_active_order(self):
