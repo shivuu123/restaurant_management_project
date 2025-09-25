67005302e2c5af6f62047ebcf2cfe2b7222839ec
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-25 10:29:46 +0530

    50604

--- models.py
+++ models.py
@@ -22,3 +22,12 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class ContactFormSubmission(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{slef.name} - {self.email}"
