d753df6d5cf5fd309dfc635182a61beb0c6a1b3f
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-25 09:54:20 +0530

    50607

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from home.models import Product
+from decimal import decimal
+from home.models import MenuItem
+
+
 
 class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -20,3 +24,17 @@ class Order(models.Model):
 class ActiveOrderManager(models.Manager):
     def get_active_order(self):
         return self.filter(status__in=['pending','processing'])
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE,related_name='items')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.menu_item.name}"
+
+    def save(self, *args, **kwargs):
+        if not self.price:
+            self.price = self.menu_item.price
+        super().save(*args, **kwargs)
