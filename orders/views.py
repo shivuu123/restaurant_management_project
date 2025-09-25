32604565b67f1af1c0c0926cb3c8bd5ee54aa802
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-25 10:12:08 +0530

    50605

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from .utils import send_email
 
-# Create your views here.
+@api_view[('POST')]
+def notify_customer(request):
+    recipient = request.data,get('email')
+    if not recipient:
+        return Response({"error": "Email is required"}, status=status.HTTP_400_BAD_REQUEST)
+    if send_email(recipient, "Order Update", "Your order has been shipped!"):
+        return Response({"message": "Email sent successfully"})
+    else:
+        return Response({"error": "Failed to send email"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 def menu_home(request):
     return render(request, 'menu.html')
