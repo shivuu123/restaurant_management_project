f409ead48c360458db1541d516eeeb855d40aca3
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-09-26 14:47:43 +0530

    51229

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_length=6, decimal_places=2)
     image = models.imageField(upload_to='menu_images/', blank=True, null=True)
     is_daily_special = models.BooleanField(default=False)
