8b2e712c5e1cfc6f4784495aa4e61e39afd00c27
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-01 10:34:00 +0530

    52684

--- home/views.py
+++ home/views.py
@@ -3,6 +3,9 @@ from rest_framework.response import APIView
 from rest_framework.response import Response
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIview
+from .models import Table
+from .serializers import TableSerializer
 
 class MenuItemByCategoryView(APIView):
     def get(self, request):
@@ -11,3 +14,14 @@ class MenuItemByCategoryView(APIView):
             return Response({"error": "Category is required."}, status=400)
         items = MenuItem.objects.filter(category__name__iexact=category)
         return Response(MenuItemSerializer(items, many=True).data)
+
+    
+class AvailableTablesAPIView(ListAPIView):
+    serializer_class = TableSerializer
+
+    def get_queryset(self):
+        return Table.objects.filter(is_available=True)
+
+class TableDetailAPIView(RetrieveAPIview):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
