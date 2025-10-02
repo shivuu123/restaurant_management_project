5ea8d503927820e37d90d5eb735c60306eb1c3fd
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-02 10:15:53 +0530

    53071

--- models.py
+++ models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import MenuItem
-
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
