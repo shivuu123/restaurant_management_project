6bfb62d2c62623e3ecebec698dd3dfd3955ec88f
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-02 10:06:57 +0530

    53072

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 from .models import Table
+from .models import MenuCategory
 
 class TableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Table
         fields = ["table_number", "capacity", "is_available"]
+
+class MenuCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuCategory
+        fields = ['id', 'name']
