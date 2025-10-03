71d7be3e488d7450ff9dfd67e2fb61aeab62f3f3
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-03 10:04:01 +0530

    53068

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
 from .utils import send_email
-
+from .models import Order
 @api_view(['POST'])
 def notify_customer(request):
     recipient = request.data.get('email')
