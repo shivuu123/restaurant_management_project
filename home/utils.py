2e41bbf57be4315f85e30f27301c9def1d9dfa70
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-08 09:22:12 +0530

    53833

--- home/utils.py
+++ home/utils.py
@@ -11,3 +11,8 @@ def is_restaurant_open():
         open_time, close_time = datetime.time(10, 0), datetime.time(23, 0)
 
     return open_time <= time <= close_time
+
+def calculate_total_price(order_items):
+    if not order_items:
+        return 0.0
+    return round(sum(item.get('quatity', 0) * item.get('price', 0) for item in order_items), 2)
