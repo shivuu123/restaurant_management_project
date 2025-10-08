4934356080cebea0ac25f86dd8bca0a3f77739f5
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-08 09:23:03 +0530

    53833

--- home/utils.py
+++ home/utils.py
@@ -1,17 +1,5 @@
 import datetime
 
-def is_restaurant_open():
-    now = datetime.datetime.now()
-    day = now.weekday()
-    time = now.time()
-
-    open_time, close_time = datetime.time(9, 0), datetime.time(22, 0)
-
-    if day >= 5:
-        open_time, close_time = datetime.time(10, 0), datetime.time(23, 0)
-
-    return open_time <= time <= close_time
-
 def calculate_total_price(order_items):
     if not order_items:
         return 0.0
