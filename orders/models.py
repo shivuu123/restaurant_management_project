eefd41bb1d4a75a078c3e9509f5fbebe5ef38baf
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-10 09:53:11 +0530

    54536

--- orders/models.py
+++ orders/models.py
@@ -32,11 +32,13 @@ class Order(models.Model):
         ('COMPLETED', 'Completed'),
     ]
 
+
     customer = models.ForeignKey('account.Customer', on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    short_id = models.CharField(max_length=20, unique=True)
     
     def __str__(self):
-        return f"Order {self.order_id} - {self.status}"
+        return f"Order {self.short_id} - {self.status}"
         
     @staticmethod
     def calculate_total_price(self):
