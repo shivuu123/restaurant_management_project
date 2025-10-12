acd1d7a10d14bd943b5ce7ce4ff62ef90259bd73
shivuu123 <145771033+shivuu123@users.noreply.github.com>
2025-10-12 12:20:52 +0530

    54535

--- models.py
+++ models.py
@@ -101,4 +101,20 @@ class Restaurant(models.Model):
         from .models import MenuItem
         return MenuItem.objects.count()
 
+class OpeningHours(models.Model):
+    DAY_CHOICES = [
+        ('Monday', 'Monday')
+        ('Tuesday', 'Tuesday')
+        ('Wednesday', 'Wednesday')
+        ('Thursday', 'Thursday')
+        ('Friday', 'Friday')
+        ('Saturday', 'Saturday')
+        ('Sunday', 'Sunday')
+
+    ]
+    day = models.CharField(max_length=10, choices=DAY_CHOICES, unique=True)
+    Opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    def __str__(self):
+        return f"(self.day): {self.opening_time} - {self.closing_time}"
 
